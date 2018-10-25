a9acfadc04b55813877e724fe94c945716f329d8
Lucas Kohorst <kohorstlucas@gmail.com>
2018-10-25 08:15:52 -0400

    Added Functionality
    - Allow user to input location, part and
    if they want to filter by price range
    -Created a new column in the spreadsheet to get the price of a part
    -Updated the element value for the next page button so that it works (used xpath)
    -Prints "No more results for the item" when there are no more  results left to  scrape

--- AutoPartFinder.py
+++ AutoPartFinder.py
@@ -6,16 +6,18 @@ import math
 import time
 
 #The part i'd like to find in AutoAndParts
-part = "subaru"
-location = "sfbay"
+print("Enter the location: ")
+location = input()
 
-#open webbrowser
-driver = webdriver.Firefox()
+print ("Enter the part you are looking for: ")
+part = input()
+
+driver = webdriver.Chrome('/Library/Python/2.7/site-packages/chromedriver')
 driver.get("https://" + location + ".craigslist.org/search/pts")
-elem = driver.find_element_by_class_name("dropdown-list")
-elem.click()
 
 #change to list view
+elem = driver.find_element_by_class_name("dropdown-list")
+elem.click()
 elem = driver.find_element_by_id("listview")
 elem.click()
 
@@ -24,15 +26,27 @@ elem = driver.find_element_by_id("query")
 elem.send_keys(part)
 elem.submit()
 
-filename = "CLPartFinder.csv"
+print("Do you want to enter a price range? (Y/N) ")
+priceRange = input()
+
+if (priceRange.lower() == "y"):
+    print("Enter the lower price: ")
+    lowerPrice = input()
+    print("Enter the upper price: ")
+    upperPrice = input()
+    driver.find_element_by_name('min_price').send_keys(lowerPrice)
+    driver.find_element_by_name('max_price').send_keys(upperPrice)
+    driver.find_element_by_xpath(
+        '//*[@id="searchform"]/div[2]/div/div[7]/button')
+
+filename = (part + ".csv")
 f = open(filename, "w")
-headers = "#, Date and Time, Title, Link\n"
+headers = "#, Date and Time Posted, Title, Link, Price\n"
 f.write(headers)
 
-noNextPage = 0 #true or false for nextPage
 TotalCount = 1
 
-while(noNextPage == 0):
+while(True):
     time.sleep(5)
     page = uReq(driver.current_url)
     pageHTML = page.read()
@@ -42,26 +56,26 @@ while(noNextPage == 0):
     postDates = page_soup.find_all('time', 'result-date')
     postTitles = page_soup.find_all('a', 'result-title hdrlnk')
     postLinks = page_soup.find_all('a', 'result-title hdrlnk')
+    postPrice = page_soup.find_all('span', 'result-price')
 
     for j in range(0, len(postDates)):
         date = postDates[j].get('title')
         title = postTitles[j].text
         link = postLinks[j].get('href')
+        price = postPrice[j].text
 
-        line = str(TotalCount) + ',' + date + ',' + title.replace(',','|') + ',' + link + "\n"
+        line = str(TotalCount) + ',' + date + ',' + title.replace(',','|') + ',' + link + "," + price + "\n"
         TotalCount+=1
         f.write(line)
 
     try:
-        elem = driver.find_element_by_class_name('button.next')
-        elem.click()
+        #Clicking the next button
+        driver.find_element_by_xpath(
+            '//*[@id="searchform"]/div[5]/div[3]/span[2]/a[3]').click()
 
-    except ElementNotInteractableException:
-        noNextPage = 1
+    except:
+        print ("No more results for the item: " + part)
+        break
 
 f.close()
-
 driver.quit()
-
-
-
